c179ed2f6c15e13f9e63fa11a860ab3a6afd3380
Wayne Brantley <wayne@waynebrantley.com>
2015-03-20 17:54:34 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i "s|<proxy_pass_ip>|$YOUTRACK_PORT_80_TCP_ADDR|" /etc/nginx/nginx_es.conf
-sed -i "s|<proxy_pass_port>|$YOUTRACK_PORT_80_TCP_PORT|" /etc/nginx/nginx_es.conf
+sed -i "s|<proxy_pass_ip>|$YOUTRACK_PORT_80_TCP_ADDR|" /etc/nginx/nginx_yt.conf
+sed -i "s|<proxy_pass_port>|$YOUTRACK_PORT_80_TCP_PORT|" /etc/nginx/nginx_yt.conf
 
 nginx
