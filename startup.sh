acd5bfa91ccf9ca97de84add1aee0bfef07ee8cc
Wayne Brantley <wayne@miniter.com>
2015-10-15 23:28:07 -0400

    add and use original config file

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#copy original config over each time docker starts up as the ports/address change!
+/bin/cp -f /etc/nginx/nginx_yt.conf.original /etc/nginx/nginx_yt.conf
 sed -i "s|<proxy_pass_ip>|$YOUTRACK_PORT_8080_TCP_ADDR|" /etc/nginx/nginx_yt.conf
 sed -i "s|<proxy_pass_port>|$YOUTRACK_PORT_8080_TCP_PORT|" /etc/nginx/nginx_yt.conf
 
